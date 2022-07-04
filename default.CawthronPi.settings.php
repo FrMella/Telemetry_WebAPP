cfc852145973fec284341b88eb3d6dc9c39af4e7
FrMella <francisco.mella.torres@outlook.com>
2022-07-05 11:59:14 +1200

    first commit

---
+++ default.CawthronPi.settings.php
@@ -0,0 +1,40 @@
+// #### Default configuration of Proyect ####
+// ### Redis and MySQL ###
+// ### MQTT
+
+
+<?php
+
+
+
+    $server = "127.0.0.1";
+    $database = "Cawthron_temp_access"; // database name need to change
+    $username = "Cawthron_temp_access"; // user need to change
+    $database = "Cawthron_secret_password"; // password need to change
+    $port = "3306"; // default port
+
+    $dbtest = true;
+
+
+    // #### redis database support #################################
+    $redis_enable = true;
+    $redis_server = array('host' => '127.0.0.1',
+                          'port' => '6379',
+                          'auth' => '',
+                          'prefix' => 'Cawthron_');
+
+    //MQTT configuractions
+    // Suscriber topic format is rx/*
+    //  the Publisher topic format is user
+
+    $mqtt_enable = true;
+    $mqtt_server = array('host' => 'localhost',
+                         'port' => 1883,
+                         'user' => 'Cawthron_user'
+                         'Password' => 'Cawthron_password',
+                         'basetopic' => 'Cawthron_data',
+                         'client_id' => 'Cawthron_telemetry');
+
+    #
+
+
---
+++ engine.core.php
@@ -0,0 +1,179 @@
+
+
+<?php
+/*
+    ### Cawthron Telemetry proposal ###
+    #### Proof of concept engine core ###
+    is_http_request: revisa que estas accediendo desde el browser
+    get_app_path: busca el inicio del programa
+    db_check: devuelve que las tablas en la base de datos fueron creadas correctamente
+    controller: busca controlladores externos de acuerdo al patron de diseno MVC
+    view : devuelve la vista
+    get : get a http
+    post : post a http request
+    prop:
+    request_header: devuelve el encabezado del index
+
+*/
+
+defined('CAWTHRON_ENGINE') or die('RESTRICTED ACCESS');
+
+function is_http_request() {
+    if (server('HTTPS') == 'on' {
+        return true;
+    } elseif (server('HTTP_X_FORWARDED_PROTOCOL') == "https") {
+        return true;
+    } elseif (server('HTTP_X_FORWARDED_PROTO') == "https") {
+        return true;
+    }
+    return false;
+}
+
+function get_app_path($manual_domain = false) {
+    if (is_http_request()){
+        $proto = "https";
+    }else {
+        $proto = "http";
+    }
+
+    if ($manual_domain) {
+        return "$proto://".$manual_domain."/";
+    }
+    if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])){
+        $path = dirname("$proto://". server('HTTP_X_FORWARDED_HOST'). server('SCRIPT_NAME')) . "/";
+
+    }else {
+        $path = dirname("$proto://" . server('HTTP_HOST') . server ('SCRIPT_NAME')) . "/";
+    }
+
+    return $path;
+}
+
+function db_check($sql, $database){
+    $query_result = $sql->query("SELECT count(table_schemes) from information_schema.tables WHERE table_schemes = '$database'");
+    $row = $query_result->fetch_array();
+    if ($row['0']>0){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+function controller ()
+{
+    $route_output = array('content'=>EMPTY_ROUTE);
+
+    if ($controller_name) {
+        $controller = $controller_name."_controller";
+        $controllerScript = "Ext_Modules/".$controller_name."/".$controller.".php";
+        if (is_file($controllerScript)) {
+            require_once $controllerScript;
+            $route_output = $controller();
+            if (!is_array($route_output) || !isset($route_output["content"])) {
+                $route_output = array("content"=>$route_output);
+            }
+        }
+    }
+    return $route_output;
+}
+
+function view($filepath, array $args = array()) {
+    global $path;
+    $args['path'] = $path;
+    $content = '';
+    if (file_exists($filepath)) {
+        extract($args);
+        ob_start();
+        include "$filepath";
+        $content = ob_get_clean();
+    }
+    return $content;
+}
+
+function get($index, $missing_error = false, $default=null) {
+
+    $val = $default;
+    if (isset($_GET[$index])) {
+        $val = rawurldecode($_GET[$index]);
+    } else if ($missing_error) {
+        header('Content-Type: text/plain');
+        die("missing $index parameter");
+    }
+
+    $val = stripslashes($val);
+    return $val;
+}
+
+function post($index, $missing_error = false, $default=null) {
+    $value_return = $default;
+    if (isset($_POST[$index])) {
+        if (!is_array($_POST[$index])) {
+            $value_return = rawurldecode($_POST[$index]);
+        } else {
+            $SANTIZED_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            if (!empty($SANTIZED_POST[$index])) {
+                $value_return = $SANTIZED_POST[$index];
+            }
+        }
+    } else if ($missing_error) {
+        header('Content-Type: text/plain');
+        die("missing $index parameter");
+    }
+
+    if (is_array($value_return)) {
+        $value_return = array_map("stripslashes", $value_return);
+    } else {
+        $value_return = stripslashes($value_return);
+    }
+    return $value_return;
+}
+
+function prop($index, $missing_error = false, $default=null){
+    $value_return = $default;
+    if (isset($_GET[$index])) {
+        $value_return = $_GET[$index];
+    }
+    else if (isset($_POST[$index])) {
+        $value_return = $_POST[$index];
+    }
+    else if ($error_if_missing) {
+        header('Content-Type: text/plain');
+        die("missing $index parameter");
+    }
+
+    if (is_array($value_return)) {
+        $value_return = array_map("stripslashes", $value_return);
+    } else {
+        $value_return = stripslashes($value_return);
+    }
+    return $value_return;
+}
+
+function request_header($index) {
+    $value_return = null;
+    $headers = apache_request_headers();
+    if (isset($headers[$index])) {
+        $value_return = $headers[$index];
+    }
+    return $value_return;
+}
+
+function server()   {}
+
+function delete() {}
+function version() {}
+
+function load_db_scheme(){}
+
+function load_menu(){}
+
+function http_request(){}
+
+function system_error(){}
+
+function call_hook() {}
+
+function get_client_ip_env() {}
+
+function generate_secure_key(){}
+
